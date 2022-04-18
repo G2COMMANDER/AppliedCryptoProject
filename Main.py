d523e62c1929915180af4d3853bbcfcb4658690c
Christian M <cmoticska@gmail.com>
2022-04-18 12:31:38 -0400

    Actually started the base code

--- Main.py
+++ Main.py
@@ -1,4 +1,17 @@
-test = 12
-word = "testing again"
-print("Heyo Gamers", test, word)
-print("test TWO")
+# Stream Cipher Analysis
+# Calvin Hariprasad, Cameron Ugi, Christian Moticska
+
+import os
+import sys
+
+# Step 1: Obtaining plaintext
+file = open(os.path.join(sys.path[0], "plaintext.txt"), "r")
+plaintext = file.read()
+
+# Step 2: Convert plaintext to decimal
+# plaintext_decimal = float(plaintext.txt)
+
+# print(plaintext_decimal)
+print(plaintext)
+
+file.close()
