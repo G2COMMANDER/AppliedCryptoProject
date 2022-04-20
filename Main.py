3185dba1b97c3869e41fab28405141867641e5a6
Christian M <cmoticska@gmail.com>
2022-04-20 11:47:07 -0400

    Added encryption for 16 key

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 # Stream Cipher Analysis
 # Calvin Hariprasad, Cameron Ugi, Christian Moticska
 
+import os
 import sys
-import random
+from os import urandom
 
 # Step 1: Obtaining 128 bit hex plaintext
 
@@ -16,7 +17,23 @@ i = int(plaintext, 16)
 plaintext_decimal = str(i)
 print(plaintext_decimal)
 
-# Step 3: Generate and bitwise XOR encode with a 16 bit key
+# Step 3: Generate 16 bit key and bitwise XOR encode
 
-key_16 =
-print(key_16)
+
+def xor_strings(s, t) -> bytes:
+    """xor two strings together."""
+    if isinstance(s, str):
+        # Text strings contain single characters
+        return b"".join(chr(ord(a) ^ ord(b)) for a, b in zip(s, t))
+    else:
+        # Bytes objects contain integer values in the range 0-255
+        return bytes([a ^ b for a, b in zip(s, t)])
+
+
+key_16 = urandom(16)
+print('Key:', key_16)
+
+CipherText = xor_strings(plaintext_decimal.encode('utf8'), key_16)
+print("CipherText:", CipherText)
+
+# Step 4: Generate 128 bit key (one time pad) and bitwise XOR encode
