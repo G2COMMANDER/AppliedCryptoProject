57c3afa458ba791d27b4017008d4fc0eb7890c21
Christian M <cmoticska@gmail.com>
2022-04-20 10:57:55 -0400

    Nothing significant, starting step 3

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 # Stream Cipher Analysis
 # Calvin Hariprasad, Cameron Ugi, Christian Moticska
 
-import os
 import sys
+import random
 
 # Step 1: Obtaining 128 bit hex plaintext
 
@@ -11,7 +11,12 @@ plaintext = file.read()
 file.close()
 
 # Step 2: Convert plaintext to decimal
+
 i = int(plaintext, 16)
 plaintext_decimal = str(i)
-
 print(plaintext_decimal)
+
+# Step 3: Generate and bitwise XOR encode with a 16 bit key
+
+key_16 =
+print(key_16)
