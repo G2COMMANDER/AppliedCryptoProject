14ca180ba13d28235a6c004ee41b8ba01c290d1b
Christian M <cmoticska@gmail.com>
2022-04-21 22:32:12 -0400

    Finished

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
-# XOR Encryption Algorithm - www.101computing.net/xor-encryption-algorithm/
-import os
-import sys
+# Stream Cipher Analysis
+# Calvin Hariprasad, Cameron Ugi, Christian Moticska
+import random
+import string
 
 
 def binary(num, length=8):
@@ -9,7 +10,13 @@ def binary(num, length=8):
     return b
 
 
-def xorEncrypt(key):
+def generateKey(n):  # Generates "x" bit password
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(n))
+    return password
+
+
+def xorEncrypt(key):  # encrypts the plaintext with the "x" bit key
     keyLength = len(key)
     cipherBin = ""
 
@@ -21,17 +28,32 @@ def xorEncrypt(key):
     return cipherBin
 
 
-file = open(os.path.join(sys.path[0], "128BitHexCode.txt"), "r")
-plaintext = file.read()
-file.close()
+counter = 0
 
-key_16 = "WoRd"
-key_128 = "C&F)J@NcRfUjXn2r"
-
-print("16 Bit Cipher: ", xorEncrypt(key_16))
-print("The 16 bit Cipher has:", xorEncrypt(key_16).count("1"), "1s")
-print("128 Bit Cipher:", xorEncrypt(key_128))
-
-# Count the number of 1s
-# Subtrarct the two counts
 # Repeat over a large number of sets
+for i in range(1000):
+    plaintext = generateKey(16)
+    print("Plaintext:", plaintext)
+    pass_16 = generateKey(4)
+    print("16 bit key: ", pass_16)
+    pass_128 = generateKey(16)
+    print("128 bit key:", pass_128)
+
+    # Output the ciphertext in binary and count the number of 1s
+    key_16 = xorEncrypt(pass_16)
+    print("\n16 Bit Cipher: ", key_16)
+    print("The 16 bit Cipher has: ", key_16.count("1"), "1s")
+
+    # encrypts
+    key_128 = xorEncrypt(pass_128)
+    print("128 Bit Cipher:", key_128)
+    print("The 128 bit Cipher has:", key_128.count("1"), "1s")
+    key_128 = xorEncrypt(pass_128)
+
+    # Subtrarct the two counts
+    difference = key_16.count("1") - key_128.count("1")
+    print("\n16 bit - 128 bit encryption:", difference)
+
+    counter += difference
+
+print(counter/1000)
